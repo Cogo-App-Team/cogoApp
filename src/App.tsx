9f6cf9164ee64ca0395f12403942bd2802bd6ac7
aneeltam <matleena.boral@gmail.com>
2023-10-29 10:51:34 +0200

    Merged changes from Nasim's branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,104 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+// App.tsx
+import React from 'react';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonMenu,
+  IonList,
+  IonItem,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonRouterOutlet,
+  IonMenuButton,
+  setupIonicReact,
+} from '@ionic/react';
+import { home, camera, images, settings, person } from 'ionicons/icons';
+import { Redirect, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
+import Photo from './pages/Photo';
+import Gallery from './pages/Gallery';
+import Settings from './pages/Settings';
+import Profile from './pages/Profile';
+import Login from './components/Login';
+import Registration from './components/Registration';
 
-/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <Router>
+        <IonMenu side="start" contentId="main-content">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonList>
+              <IonItem button routerLink="/photo">
+                <IonIcon icon={camera} />
+                <IonLabel>Camera</IonLabel>
+              </IonItem>
+              <IonItem button routerLink="/photo/gallery">
+                <IonIcon icon={images} />
+                <IonLabel>Gallery</IonLabel>
+              </IonItem>
+              <IonItem button routerLink="/settings">
+                <IonIcon icon={settings} />
+                <IonLabel>Settings</IonLabel>
+              </IonItem>
+              <IonItem button routerLink="/profile">
+                <IonIcon icon={person} />
+                <IonLabel>Profile</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonContent>
+        </IonMenu>
+        <IonTabs>
+          <IonRouterOutlet id="main-content">
+            <Route path="/home" component={Home} exact />
+            <Route path="/photo" component={Photo} exact />
+            <Route path="/photo/gallery" component={Gallery} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/" render={() => <Redirect to="/home" />} exact />
+            <Route path="/login" component={Login} />
+            <Route path="/registration" component={Registration} />
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="home" href="/home">
+              <IonIcon icon={home} />
+            </IonTabButton>
+            <IonTabButton tab="photo" href="/photo">
+              <IonIcon icon={camera} />
+            </IonTabButton>
+            <IonTabButton tab="gallery" href="/photo/gallery">
+              <IonIcon icon={images} />
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </Router>
+    </IonApp>
+  );
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButton,
+  IonIcon,
+  IonButtons,
+  IonMenuButton, // Add this import
+} from '@ionic/react';
+import { camera, images, settings, person } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,17 +18,40 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonMenuButton slot="start" />
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+    {/*   <IonContent>
+        <div className="ion-text-center">
+          <IonButton routerLink="/photo">
+            <IonIcon icon={camera} />
+            <br />
+            Take a Photo
+          </IonButton>
+        </div>
+        <div className="ion-text-center">
+        <IonButton routerLink="/photo/gallery">
+          <IonIcon icon={images} />
+            <br />
+            Gallery
+        </IonButton>
+        </div>
+        <div className="ion-text-center">
+          <IonButton routerLink="/settings">
+            <IonIcon icon={settings} />
+            <br />
+            Settings
+          </IonButton>
+        </div>
+        <div className="ion-text-center">
+          <IonButton routerLink="/profile">
+            <IonIcon icon={person} />
+            <br />
+            Profile
+          </IonButton>
+        </div>
+      </IonContent> */}
     </IonPage>
   );
 };
